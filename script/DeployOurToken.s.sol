c3cbed5328d36c1ba965f5478aa9c438eaf83853
wildanfh <wildanf2005@gmail.com>
2026-03-13 16:19:53 +0100

    deploy erc20

---
+++ script/DeployOurToken.s.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+
+contract DeployOurToken is Script {
+  uint256 constant INITIAL_SUPPLY = 1000 ether;
+  function run() external {
+    vm.startBroadcast();
+    new OurToken(INITIAL_SUPPLY);
+    vm.stopBroadcast();
+  }
+}
